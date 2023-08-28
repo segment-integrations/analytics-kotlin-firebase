78c73c84dac75d39e95e38cba8341cc76242462b
Michael Grosse Huelsewiesche <mihuelsewiesche@twilio.com>
2023-08-28 15:01:41 -0400

    Create release 1.5.4.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
 
-    implementation("com.segment.analytics.kotlin:android:1.5.0")
+    implementation("com.segment.analytics.kotlin:android:1.8.0")
     implementation("androidx.multidex:multidex:2.0.1")
 
     implementation("androidx.core:core-ktx:1.7.0")
@@ -56,7 +56,7 @@ dependencies {
 
 // Partner Dependencies
 dependencies {
-    implementation("com.google.firebase:firebase-analytics-ktx:20.1.0")
+    implementation("com.google.firebase:firebase-analytics-ktx:21.3.0")
 }
 
 // Test Dependencies
